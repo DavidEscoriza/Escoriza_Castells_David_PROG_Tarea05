a2422d1c31f2be1781c1a3ad9aa1f0d4d7d94a89
David <David@Davi-PC>
2018-02-03 20:45:38 +0100

    Creamos la clase Turismo con los atributos y visibilidad adecuados.

---
+++ Escoriza_Castells_David_PROG_Tarea05/src/tarea05/modelo/Turismo.java
@@ -0,0 +1,14 @@
+package tarea05.modelo;
+
+/**
+ *
+ * @author David
+ */
+public class Turismo {
+    private String matricula, marca, modelo;
+    private int cilindrada;
+    private boolean disponible;
+    
+    
+    
+}
