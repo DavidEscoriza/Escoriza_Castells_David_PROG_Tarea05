dd04504018434d9691a06e24f709ed37dc42cd09
David <David@Davi-PC>
2018-02-03 21:34:33 +0100

    Creamos la clase Alquiler con los atributos y visibilidad adecuados.

---
+++ Escoriza_Castells_David_PROG_Tarea05/src/tarea05/modelo/Alquiler.java
@@ -0,0 +1,25 @@
+package tarea05.modelo;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author David
+ */
+public class Alquiler {
+    private Cliente cliente;
+    private Turismo turismo;
+    private Date fecha;
+    private int dias;
+    
+    private final SimpleDateFormat FORMATO_FECHA = new SimpleDateFormat ("dd/mm/yyyy HH:mm");
+    private final int MS_DIA = 1000 * 60 * 60 * 24;
+    private final double PRECIO_DIA = 30;
+    
+    
+    
+    
+    
+    
+}
