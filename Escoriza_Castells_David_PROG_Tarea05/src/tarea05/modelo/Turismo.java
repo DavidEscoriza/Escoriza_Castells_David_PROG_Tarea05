311ae998ccc1de63943cd044fce34e3c5a920a79
David <David@Davi-PC>
2018-02-03 21:08:22 +0100

    Creamos los construcctores copia y por parametros de la clase Turismo y el método compruebaMatricula.

--- Escoriza_Castells_David_PROG_Tarea05/src/tarea05/modelo/Turismo.java
+++ Escoriza_Castells_David_PROG_Tarea05/src/tarea05/modelo/Turismo.java
@@ -1,5 +1,8 @@
 package tarea05.modelo;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author David
@@ -9,6 +12,29 @@ public class Turismo {
     private int cilindrada;
     private boolean disponible;
     
+    public Turismo (String matricula, String marca, String modelo, int cilindrada){
+        if (!compruebaMatricula (matricula))
+            throw new ExcepcionAlquilerVehiculos ("La matrícula es inválida.");
+        this.matricula=matricula;
+        this.marca=marca;
+        this.modelo=modelo;
+        if (cilindrada<=0)
+            throw new ExcepcionAlquilerVehiculos ("La cilindrada debe ser mayor que 0.");
+        this.cilindrada=cilindrada;
+    }
     
+    public Turismo(Turismo otroTurismo){
+        matricula=otroTurismo.matricula;
+        marca=otroTurismo.marca;
+        modelo=otroTurismo.modelo;
+        cilindrada=otroTurismo.cilindrada;
+        disponible=otroTurismo.disponible;
+    }
+    
+    private boolean compruebaMatricula(String matricula){
+        Pattern p = Pattern.compile("[0-9]{4}[B-DF-HJ-NP-TV-Z]{3}");
+        Matcher m=p.matcher(matricula);
+        return m.matches();
+    }
     
 }
