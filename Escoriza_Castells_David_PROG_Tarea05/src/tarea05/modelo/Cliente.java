3ff49f730e5ec9fa7218c2c7907bc69522b84c44
David <David@Davi-PC>
2018-02-03 20:27:21 +0100

    Generamos un constructor con parámetros que compruebe que el DNI y el código postal son válidos, con los métodos compruebaDni y compruebaCodigoPostal. Además creamos un contrusctor copia.

--- Escoriza_Castells_David_PROG_Tarea05/src/tarea05/modelo/Cliente.java
+++ Escoriza_Castells_David_PROG_Tarea05/src/tarea05/modelo/Cliente.java
@@ -1,8 +1,52 @@
 package tarea05.modelo;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Cliente {
     private String nombre, dni, telefono, direccion, localidad, codigoPostal;
     private int identificador;
     private static int numClientes=0;
     
+    
+    public Cliente(String nombre, String dni, String telefono, String direccion, String localidad, String codigoPostal){
+        this.nombre=nombre;
+        if (!compruebaDni(dni))
+            throw new ExcepcionAlquilerVehiculos("DNI inválido");
+        this.dni=dni;
+        this.telefono=telefono;
+        this.direccion=direccion;
+        this.localidad=localidad;
+        if (!compruebaCodigoPostal(codigoPostal))
+            throw new ExcepcionAlquilerVehiculos("Código postal inválido");
+        this.codigoPostal=codigoPostal;
+        numClientes++;
+        identificador = numClientes;
+    }
+    
+    public Cliente(Cliente otroCliente){
+        nombre=otroCliente.nombre;
+        dni=otroCliente.dni;
+        telefono=otroCliente.telefono;
+        direccion=otroCliente.direccion;
+        localidad=otroCliente.localidad;
+        codigoPostal=otroCliente.codigoPostal;
+        identificador=otroCliente.identificador;
+    }
+    
+    private boolean compruebaDni(String dni){
+        Pattern p=Pattern.compile("[0-9]{8}[A-Z]");
+        Matcher m;
+        
+        m=p.matcher(dni);
+        return m.matches();
+    }
+    
+    private boolean compruebaCodigoPostal(String codigoPostal){
+    Pattern p=Pattern.compile("[0-9]{5}");
+    Matcher m;
+        
+    m=p.matcher(codigoPostal);
+    return m.matches();
+    }
 }
