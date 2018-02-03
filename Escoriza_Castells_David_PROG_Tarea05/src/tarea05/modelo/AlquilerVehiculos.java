a134852112cd4dc129eff6e3d46214240d7a3f9a
David <David@Davi-PC>
2018-02-03 22:15:12 +0100

    Creamos un constructor por defecto que nos genere los arrays de clientes, turismos y alquileres. Modificamos tambien las constantes de los máximos.

--- Escoriza_Castells_David_PROG_Tarea05/src/tarea05/modelo/AlquilerVehiculos.java
+++ Escoriza_Castells_David_PROG_Tarea05/src/tarea05/modelo/AlquilerVehiculos.java
@@ -5,11 +5,24 @@ package tarea05.modelo;
  * @author David
  */
 public class AlquilerVehiculos {
-    private Cliente cliente;
-    private Turismo turismo;
-    private Alquiler alquiler;
+    private Cliente[] clientes;
+    private Turismo[] turismos;
+    private Alquiler[] alquileres;
     
-    private final int MAX_CLIENTES=Integer.MAX_VALUE;
-    private final int MAX_TURISMO=Integer.MAX_VALUE;
-    private final int MAX_ALQUILER=Integer.MAX_VALUE;
+    private final int MAX_CLIENTES=30;
+    private final int MAX_TURISMOS=30;
+    private final int MAX_ALQUILERES=30;
+
+    public AlquilerVehiculos(){
+        clientes = new Cliente[MAX_CLIENTES];
+	turismos= new Turismo[MAX_TURISMOS];
+	alquileres = new Alquiler[MAX_ALQUILERES];
+    }
+    
+    
+
+
+
+
+
 }
