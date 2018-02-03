3c306210ba89f72f416de0cf08bb2cd12fd53dcf
David <David@Davi-PC>
2018-02-03 20:20:39 +0100

    Creamos la clase Cliente con sus atributos y visibilidad adecuados.

---
+++ Escoriza_Castells_David_PROG_Tarea05/src/tarea05/modelo/Cliente.java
@@ -0,0 +1,8 @@
+package tarea05.modelo;
+
+public class Cliente {
+    private String nombre, dni, telefono, direccion, localidad, codigoPostal;
+    private int identificador;
+    private static int numClientes=0;
+    
+}
