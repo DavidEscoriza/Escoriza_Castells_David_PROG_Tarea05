c9c5b27b68ad0a8677b94eb7af3055edf5f587fd
David <David@Davi-PC>
2018-02-03 22:10:44 +0100

    Creamos la clase AlquilerVehiculos con sus atributos y visibilidad adecuados.

---
+++ Escoriza_Castells_David_PROG_Tarea05/src/tarea05/modelo/AlquilerVehiculos.java
@@ -0,0 +1,15 @@
+package tarea05.modelo;
+
+/**
+ *
+ * @author David
+ */
+public class AlquilerVehiculos {
+    private Cliente cliente;
+    private Turismo turismo;
+    private Alquiler alquiler;
+    
+    private final int MAX_CLIENTES=Integer.MAX_VALUE;
+    private final int MAX_TURISMO=Integer.MAX_VALUE;
+    private final int MAX_ALQUILER=Integer.MAX_VALUE;
+}
